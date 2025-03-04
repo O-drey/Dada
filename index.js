becd503c3830681de5546a4f0702736e0971d586
Audrey Carmont <carmont.audrey@gmail.com>
2025-03-04 17:28:08 +0100

    FIX : Rivera + dogs playing multiple times DEV : curtains drag

--- index.js
+++ index.js
@@ -41,6 +41,10 @@ const observeDogs = () => {
 
           setTimeout(() => audioDogs.pause(), 4000)
           console.log("is playing dogs barking")
+
+          if (element.intersectionRatio > 0) {
+            observer.unobserve(element.target)
+          }
         }
       })
     },
@@ -55,22 +59,26 @@ const observeDogs = () => {
 
 const observeRivera = () => {
   const observer = new IntersectionObserver(
-    (entries) => {
+    (entries, observer) => {
       entries.forEach((element) => {
         if (element.isIntersecting) {
           element.target.style.visibility = "visible"
 
-          setTimeout(() => {
-            const text = document.createElement("p")
-            text.className = "shouting-text"
-            text.innerHTML = "Y'all better quiet down!"
-            divImgRivera.appendChild(text)
+          // setTimeout(() => {
+          const text = document.createElement("p")
+          text.className = "shouting-text"
+          text.innerHTML = "Y'all better quiet down!"
+          divImgRivera.appendChild(text)
 
-            text.style.rotate = "-10deg"
+          text.style.rotate = "-10deg"
+          text.style.right = "20%"
 
-            audioRivera.play()
-            observeDogs()
-          }, 2500)
+          audioRivera.play()
+          observeDogs()
+          // }, 2500)
+          if (element.intersectionRatio > 0) {
+            observer.unobserve(element.target)
+          }
         }
       })
     },
@@ -85,8 +93,34 @@ const observeRivera = () => {
 
 observeRivera()
 
-window.addEventListener("load", () => {
-  window.addEventListener("click", () => {
-    return
+// CURTAINS
+const curtains = document.getElementById("curtains")
+const curtainsImg = [
+  "./assets/imgs/curtain-left.png",
+  "./assets/imgs/curtain-right.png",
+]
+
+// curtainsImg.forEach((curtain) => {
+//   curtains.appendChild(curtain)
+// })
+
+const curtainRight = document.getElementById("curtain-right")
+const curtainLeft = document.getElementById("curtain-left")
+const curtainAudio = new Audio()
+curtainAudio.src = "./assets/sounds/curtain-slide-pull-05.wav"
+const mouseDrag = () => {
+  curtainRight.addEventListener("drag", () => {
+    curtainAudio.play()
   })
-})
+  curtainLeft.addEventListener("drag", () => {
+    curtainAudio.play()
+  })
+  curtainLeft.addEventListener("dragleave", () => {
+    curtains.style.visibility = "hidden"
+  })
+  curtainRight.addEventListener("dragleave", () => {
+    curtains.style.visibility = "hidden"
+  })
+}
+
+mouseDrag()
